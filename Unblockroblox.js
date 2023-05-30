faa9613f3d8dae86de26b95e1018f767c267a366
Nicolas M <127147029+NicolasT3ecoder@users.noreply.github.com>
2023-05-30 09:26:23 -0400

    Unblockroblox.js
    unblocks roblox if your on a school chromebook

---
+++ Unblockroblox.js
@@ -0,0 +1,15 @@
+javascript:
+alert("Working");
+alert("roblox configurating!");
+parent.alert(ErrorEvent) = true;
+alert("ERROR 504");
+parent.alert = Set("localService") = true;
+
+parent.localStorage = set("LocalDate").Exp
+connect, LocalDate = EXP(9/2/24);
+
+localUrl = ("roblox.com").set;
+HTMLOptionsCollection = null.collect;
+
+URL.apply("arguments") = false;
+Set.alertParent = set-null;
